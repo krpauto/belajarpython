19a5491d212b74283a06bc00fd081998a631a47b
Kevin Rivaldo Panggabean <82790760+krpauto@users.noreply.github.com>
2022-04-13 20:57:12 +0700

    Create kurang.py

---
+++ kurang.py
@@ -0,0 +1,10 @@
+def kurang(angka1: float, angka2: float) -> float:
+    """
+    hasil dari pengurangan antara angka1 dan angka2
+    >>> kurang(10.0, 9.0)
+    5.0
+    """
+    return angka1 - angka2
+
+
+print(kurang(10, 9))
