56995ee494528f04fbb3b71e12727ed5cf3b1a35
Kevin Rivaldo Panggabean <82790760+krpauto@users.noreply.github.com>
2022-04-13 20:54:41 +0700

    Create penjumlahan.py

---
+++ penjumlahan.py
@@ -0,0 +1,8 @@
+def tambah(angka1: float, angka2: float) -> float:
+    """
+    hasil dari pertambahan antara angka1 dan angka2
+    >>> tambah(3.0, 2.0)
+    5.0
+    """
+    return angka1 + angka2
+print(tambah(10, 9))
